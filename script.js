65b4c5b2980186add94e6330ada195039dd5042d
Moe Mousavi <99014654+moeIsAwesome@users.noreply.github.com>
2022-08-16 14:34:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const background = document.getElementById('background');
 password.addEventListener('input', (e) => {
   const input = e.target.value;
   const length = input.length;
-  const blurValue = 1 + length * 1.04;
+  const blurValue = 1 + length * 1.02;
   background.style.filter = `blur(${blurValue}px)`;
 });
